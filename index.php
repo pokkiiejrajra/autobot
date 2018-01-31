378a48bb06815c8e7de972c097688c421552c01e
someone <roppy63@hotmail.com>
2018-01-31 16:13:47 +0700

    Initial

--- index.php
+++ index.php
@@ -9,7 +9,9 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 $channel_token = 'CRzQgIiN8vaAyB3HqUqg9Zt5h6r5kyPjUyAbri9sZVhTYtpSdIryoutQ74v3aubtYfSOe5OmDyd6zbI9T1bIpcTAbwHHK+2T1O93tVQowCYFfgT5AyO+o66nG2izp2dCyq76X+j0G+TVR6bPzzLygAdB04t89/1O/w1cDnyilFU='; 
 $channel_secret = 'a812f47e56a519ed75ee4f47cb924f19'; 
  
-// Get message from Line API $content = file_get_contents('php://input'); $events = json_decode($content, true); 
+// Get message from Line API 
+$content = file_get_contents('php://input'); 
+$events = json_decode($content, true); 
  
 if (!is_null($events['events'])) {     // Loop through each event     
 foreach ($events['events'] as $event) {         //  Line API send a lot of event type, we interested in message only.         
