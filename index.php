a5c6c2d89cd492a38cb175ad5f6a7250e981645c
someone <roppy63@hotmail.com>
2018-02-04 13:18:26 +0700

    initial

--- index.php
+++ index.php
@@ -2,34 +2,27 @@
 require_once('./vendor/autoload.php'); 
     
 // Namespace y
-use \LINE\LINEBot\HTTPClient\CurlHTTPClient; 
-use \LINE\LINEBot; 
-use \LINE\LINEBot\MessageBuilder\TextMessageBuilder; 
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Monolog\Handler\FirePHPHandler;
  
 $channel_token = 'CRzQgIiN8vaAyB3HqUqg9Zt5h6r5kyPjUyAbri9sZVhTYtpSdIryoutQ74v3aubtYfSOe5OmDyd6zbI9T1bIpcTAbwHHK+2T1O93tVQowCYFfgT5AyO+o66nG2izp2dCyq76X+j0G+TVR6bPzzLygAdB04t89/1O/w1cDnyilFU='; 
 $channel_secret = 'a812f47e56a519ed75ee4f47cb924f19'; 
  
-// Get message from Line API 
-$content = file_get_contents('php://input'); 
-$events = json_decode($content, true); 
-if (!is_null($events['events'])) {     // Loop through each event     
-foreach ($events['events'] as $event) {         //  Line API send a lot of event type, we interested in message only.         
-if ($event['type'] == 'message') {                
-switch($event['message']['type']) {                  
-case 'text':                       // Get replyToken                      
- $replyToken = $event['replyToken']; 
- 
-                      // Reply message                       
- $respMessage = 'Hello, your message is '. $event['message']['text']; 
- 
-                      $httpClient = new CurlHTTPClient($channel_token);                       
-                      $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));                       
-                      $textMessageBuilder = new TextMessageBuilder($respMessage);                       
-                      $response = $bot->replyMessage($replyToken, $textMessageBuilder); 
-                 break; 
-            } 
-        } 
-    } 
-} 
-
+$logger = new Logger('LineBot');
+$logger->pushHandler(new StreamHandler('php://stderr', Logger::DEBUG));
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV["LINEBOT_ACCESS_TOKEN"]);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV["LINEBOT_CHANNEL_SECRET"]]);
+$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+try {
+  $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+} catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
+  error_log('parseEventRequest failed. InvalidSignatureException => '.var_export($e, true));
+} catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
+  error_log('parseEventRequest failed. UnknownEventTypeException => '.var_export($e, true));
+} catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
+  error_log('parseEventRequest failed. UnknownMessageTypeException => '.var_export($e, true));
+} catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
+  error_log('parseEventRequest failed. InvalidEventRequestException => '.var_export($e, true));
+}
 echo 'OK';
