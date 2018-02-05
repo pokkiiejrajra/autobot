727ce42871f4741b794f579c3f02422363e434f3
pokkiiejrajra <roppy63@hotmail.com>
2018-02-05 12:35:31 +0700

    initial

--- index.php
+++ index.php
@@ -6,9 +6,15 @@ use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot; 
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder; 
  
-$channel_token = 'CRzQgIiN8vaAyB3HqUqg9Zt5h6r5kyPjUyAbri9sZVhTYtpSdIryoutQ74v3aubtYfSOe5OmDyd6zbI9T1bIpcTAbwHHK+2T1O93tVQowCYFfgT5AyO+o66nG2izp2dCyq76X+j0G+TVR6bPzzLygAdB04t89/1O/w1cDnyilFU='; 
-$channel_secret = 'a812f47e56a519ed75ee4f47cb924f19'; 
- 
+public function getChannelAccessToken(){
+	$channelAccessToken = "CRzQgIiN8vaAyB3HqUqg9Zt5h6r5kyPjUyAbri9sZVhTYtpSdIryoutQ74v3aubtYfSOe5OmDyd6zbI9T1bIpcTAbwHHK+2T1O93tVQowCYFfgT5AyO+o66nG2izp2dCyq76X+j0G+TVR6bPzzLygAdB04t89/1O/w1cDnyilFU=";
+	return $channelAccessToken;
+}
+        
+public function getChannelSecret(){
+	$channelSecret = "a812f47e56a519ed75ee4f47cb924f19";
+	return $channelSecret;
+}
 // Get message from Line API 
 $content = file_get_contents('php://input'); 
 $events = json_decode($content, true); 
