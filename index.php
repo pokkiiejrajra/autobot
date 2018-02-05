e532858f5228ac35368f5c30dd04dbcff6293880
pokkiiejrajra <roppy63@hotmail.com>
2018-02-05 12:37:35 +0700

    initial

--- index.php
+++ index.php
@@ -16,26 +16,8 @@ public function getChannelSecret(){
 	return $channelSecret;
 }
 // Get message from Line API 
-$content = file_get_contents('php://input'); 
-$events = json_decode($content, true); 
- 
-if (!is_null($events['events'])) {     // Loop through each event     
-foreach ($events['events'] as $event) {         //  Line API send a lot of event type, we interested in message only.         
-if ($event['type'] == 'message') {                
-switch($event['message']['type']) {                  
-case 'text':                       // Get replyToken                      
- $replyToken = $event['replyToken']; 
- 
-                      // Reply message                       
- $respMessage = 'Hello, your message is '. $event['message']['text']; 
- 
-                      $httpClient = new CurlHTTPClient($channel_token);                       
-                      $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));                       
-                      $textMessageBuilder = new TextMessageBuilder($respMessage);                       
-                      $response = $bot->replyMessage($replyToken, $textMessageBuilder); 
-                 break; 
-            } 
-        } 
-    } 
-} 
-echo 'OK';
+require_once __DIR__ . '/lineBot.php';
+$bot = new Linebot();
+
+$text = $bot->getMessageText(); //when user send text to bot	
+$bot->reply($text); // bot reply to user
