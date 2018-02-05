6362ba83af07b03bf6da322eb1342efbe1920040
pokkiiejrajra <roppy63@hotmail.com>
2018-02-05 15:06:51 +0700

    initial

--- index.php
+++ index.php
@@ -2,19 +2,42 @@
 require_once('./vendor/autoload.php'); 
  	
 // Namespace y
+use \LINE\LINEBot\HTTPClient\CurlHTTPClient; 
+use \LINE\LINEBot; 
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder; 
  
 public function getChannelAccessToken(){
-	$channelAccessToken = "CRzQgIiN8vaAyB3HqUqg9Zt5h6r5kyPjUyAbri9sZVhTYtpSdIryoutQ74v3aubtYfSOe5OmDyd6zbI9T1bIpcTAbwHHK+2T1O93tVQowCYFfgT5AyO+o66nG2izp2dCyq76X+j0G+TVR6bPzzLygAdB04t89/1O/w1cDnyilFU=";
+	$channelAccessToken = "CRzQgIiN8vaAyB3HqUqg9Zt5h6r5kyPjUyAbri9sZVhTYtpSdIryoutQ74v3aubtYfSOe5OmDyd6zbI9T1bIpcTAbwHHK+2T1O93tVQowCYFfgT5AyO+o66nG2izp2dCyq76X+j0G+TVR6bPzzLygAdB04t89/1O/w1cDnyilFU=
+";
 	return $channelAccessToken;
 }
         
 public function getChannelSecret(){
-	$channelSecret = "a812f47e56a519ed75ee4f47cb924f19";
+	$channelSecret = "<YOUR CHANNEL SECRET>";
 	return $channelSecret;
 }
 // Get message from Line API 
-require_once __DIR__ . '/lineBot.php';
-$bot = new Linebot();
-
-$text = $bot->getMessageText(); //when user send text to bot	
-$bot->reply($text); // bot reply to user
+$content = file_get_contents('php://input'); 
+$events = json_decode($content, true); 
+ 
+if (!is_null($events['events'])) {     // Loop through each event     
+foreach ($events['events'] as $event) {         //  Line API send a lot of event type, we interested in message only.         
+if ($event['type'] == 'message') {                
+switch($event['message']['type']) {                  
+case 'text':                       // Get replyToken                      
+ $replyToken = $event['replyToken']; 
+ 
+                      // Reply message                       
+ $respMessage = 'Hello, your message is '. $event['message']['text']; 
+ 
+                      $httpClient = new CurlHTTPClient($channel_token);                       
+                      $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));                       
+                      $textMessageBuilder = new TextMessageBuilder($respMessage);                       
+                      $response = $bot->replyMessage($replyToken, $textMessageBuilder); 
+                 break; 
+            } 
+        } 
+    } 
+} 
+echo 'OK';
+?>
